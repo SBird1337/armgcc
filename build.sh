fe2fa269e9d5af545a2d81176ed8656ed72a21e8
Karathan <philipp@karathan.at>
2019-04-05 14:00:40 +0200

    fix arm build

--- build.sh
+++ build.sh
@@ -3,8 +3,12 @@ rm -rf arm-elf/*
 mkdir /arm-elf/src /arm-elf/build
 cd arm-elf-src/build && mkdir binutils-2.22 && cd binutils-2.22
 ../../src/binutils-2.22/configure --target=arm-elf --prefix=/arm-elf --disable-werror --with-cpu=arm7tdmi --with-no-thumb-interwork --with-mode=thumb --host=i686-pc-linux --build=i686-pc-linux
-make all install
+make all
+make install
 export PATH="$PATH:/arm-elf/bin"
 cd .. && mkdir gcc-2.95.1 && cd gcc-2.95.1
-../../src/gcc-2.95.1/configure --target=arm-elf --prefix=/arm-elf --disable-werror --with-cpu=arm7tdmi --with-no-thumb-interwork --with-mode=thumb --host=i686-pc-linux --build=i686-pc-linux
-make all-gcc install-gcc
+../../src/gcc-2.95.1/configure --target=arm-elf --prefix=/arm-elf --disable-werror --with-cpu=arm7tdmi --with-no-thumb-interwork --with-mode=thumb --disable-multilib --enable-languages="c" --host=i686-pc-linux --build=i686-pc-linux
+make all-gcc
+make install-gcc
+make all
+make install
